f85fd4dd32cad509f73a5618d2a404626c74aee7
Alexandre Ringuette <aring057@uottawa.ca>
2023-03-03 14:55:07 -0500

    added comments

--- Concurrent/pipeline.go
+++ Concurrent/pipeline.go
@@ -7,12 +7,14 @@ import (
 	"sync"
 )
 
+// Returns a random point in the point splice
 func RandomPoints(points []Point3D) Point3D {
 	randIndex := rand.Intn(len(points))
-
 	return points[randIndex]
 }
 
+// Generates randoms points from the provided slice of points.
+// Outputs channel transmits instances of Point3D
 func GenRandomPoints(wg *sync.WaitGroup, stop <-chan bool, points *[]Point3D, fct func([]Point3D) Point3D) <-chan Point3D {
 	pointStream := make(chan Point3D)
 
@@ -32,6 +34,8 @@ func GenRandomPoints(wg *sync.WaitGroup, stop <-chan bool, points *[]Point3D, fc
 	return pointStream
 }
 
+// Reads Point3D instances from input channel and accumulate 3 points
+// Output channel transmits arrays of Point3D
 func TripletPointGen(wg *sync.WaitGroup, stop <-chan bool, inputPointStream <-chan Point3D) <-chan []Point3D {
 	outputPointStream := make(chan []Point3D)
 
@@ -57,6 +61,8 @@ func TripletPointGen(wg *sync.WaitGroup, stop <-chan bool, inputPointStream <-ch
 	return outputPointStream
 }
 
+// Reads arrays of Point3D and resends them.
+// Automatically stops the pipeline after received N arrays
 func TakeN(wg *sync.WaitGroup, stop <-chan bool, inputPointStream <-chan []Point3D, n int) <-chan []Point3D {
 	outputPointStream := make(chan []Point3D)
 
@@ -68,7 +74,7 @@ func TakeN(wg *sync.WaitGroup, stop <-chan bool, inputPointStream <-chan []Point
 		for i := 0; i < n; i++ {
 			select {
 			case <-stop:
-				break
+				return
 			case outputPointStream <- <-inputPointStream:
 			}
 		}
@@ -77,6 +83,8 @@ func TakeN(wg *sync.WaitGroup, stop <-chan bool, inputPointStream <-chan []Point
 	return outputPointStream
 }
 
+// Reads arrays of three Point3D and compute the plane defined by these points.
+// Output channel transmits Plane3D instances.
 func PlaneEstimator(wg *sync.WaitGroup, stop <-chan bool, inputPointStream <-chan []Point3D) <-chan Plane3D {
 	outputPlaneStream := make(chan Plane3D)
 
@@ -100,6 +108,8 @@ func PlaneEstimator(wg *sync.WaitGroup, stop <-chan bool, inputPointStream <-cha
 	return outputPlaneStream
 }
 
+// Counts the number of points in the provided slice Point3D(input point cloud) that support the receied Plane3D
+// Output channel transmits the Plane3D and the number of supporting points in a Point3DwSupport instances.
 func SupportingPointsFinder(wg *sync.WaitGroup, stop <-chan bool, inputPlaneStream <-chan Plane3D, pointCloud *[]Point3D, eps float64) <-chan Plane3DwSupport {
 	outputPlaneStream := make(chan Plane3DwSupport)
 
@@ -121,6 +131,7 @@ func SupportingPointsFinder(wg *sync.WaitGroup, stop <-chan bool, inputPlaneStre
 	return outputPlaneStream
 }
 
+// Multiplexes the results received from multiple channels into one output channel
 func FanIn(wg *sync.WaitGroup, stop <-chan bool, channels []<-chan Plane3DwSupport) <-chan Plane3DwSupport {
 	var multiplexGroup sync.WaitGroup
 	outputPlaneSupportStream := make(chan Plane3DwSupport)
@@ -152,7 +163,8 @@ func FanIn(wg *sync.WaitGroup, stop <-chan bool, channels []<-chan Plane3DwSuppo
 	return outputPlaneSupportStream
 }
 
-func DominantPlaneIdentifier(inputPlaneSupportStream <-chan Plane3DwSupport, bestSupport *Plane3DwSupport) {
+// Receives Plane3DwSupport instances and keeps in memory the plane with the best support
+func DominantPlaneIdentifier(wg *sync.WaitGroup, stop <-chan bool, inputPlaneSupportStream <-chan Plane3DwSupport, bestSupport *Plane3DwSupport) {
 
 	for i := range inputPlaneSupportStream {
 		if bestSupport.SupportSize < i.SupportSize {
